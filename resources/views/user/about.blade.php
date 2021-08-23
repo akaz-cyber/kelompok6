64831b3b82943eceeae7922edcd42aadbdebbb4f
akaz-cyber <ajrisonic62@gmail.com>
2021-08-23 17:53:53 +0700

    "mengganti about"

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -37,7 +37,7 @@
     <div class="col-lg-4 mt-4 mt-lg-0">
       <div class="box" data-aos="fade-up" data-aos-delay="600">
         <img src="assets/img/kamera-3.png" class="img-fluid" alt="">
-        <h3>agar siswa atau siswi bisa lebih jago </h3>
+        <h3>agar siswa atau siswi bisa mengasa skill </h3>
         <p>jadi siswa siswi bisa mempraktekan skill photografy di luar maupun di dalam sekolah</p>
       </div>
     </div>
